f4fddca1f1f7c64d99b6fdf5e1b13e88c721ce6d
Zein1 <Zein1@users.noreply.github.com>
2018-11-13 10:32:08 +0100

    Update leplateau.sql
    Nouvelle BDD

---
+++ leplateau.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  mar. 13 nov. 2018 à 10:26
+-- Version du serveur :  10.1.35-MariaDB
+-- Version de PHP :  7.2.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `leplateau`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `avis`
+--
+
+CREATE TABLE `avis` (
+  `ID_Compte` int(11) NOT NULL,
+  `ID_Jeu` int(11) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `note` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `compte`
+--
+
+CREATE TABLE `compte` (
+  `ID_Compte` int(11) NOT NULL,
+  `passwrd` varchar(15) DEFAULT NULL,
+  `nom` varchar(30) DEFAULT NULL,
+  `prenom` varchar(30) DEFAULT NULL,
+  `age` varchar(30) DEFAULT NULL,
+  `sexe` int(1) DEFAULT NULL,
+  `mail` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `jeu`
+--
+
+CREATE TABLE `jeu` (
+  `nom` varchar(100) DEFAULT NULL,
+  `prix` int(10) DEFAULT NULL,
+  `nbMinJoueurs` int(2) NOT NULL,
+  `noteRedac` int(2) NOT NULL,
+  `nbMaxJoueurs` int(2) DEFAULT NULL,
+  `catégorie` varchar(30) DEFAULT NULL,
+  `ID_Jeu` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `avis`
+--
+ALTER TABLE `avis`
+  ADD KEY `ID_Compte` (`ID_Compte`),
+  ADD KEY `ID_Jeu` (`ID_Jeu`);
+
+--
+-- Index pour la table `compte`
+--
+ALTER TABLE `compte`
+  ADD PRIMARY KEY (`ID_Compte`);
+
+--
+-- Index pour la table `jeu`
+--
+ALTER TABLE `jeu`
+  ADD PRIMARY KEY (`ID_Jeu`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `compte`
+--
+ALTER TABLE `compte`
+  MODIFY `ID_Compte` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `jeu`
+--
+ALTER TABLE `jeu`
+  MODIFY `ID_Jeu` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `avis`
+--
+ALTER TABLE `avis`
+  ADD CONSTRAINT `avis_ibfk_1` FOREIGN KEY (`ID_Compte`) REFERENCES `compte` (`ID_Compte`),
+  ADD CONSTRAINT `avis_ibfk_2` FOREIGN KEY (`ID_Jeu`) REFERENCES `jeu` (`ID_Jeu`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
