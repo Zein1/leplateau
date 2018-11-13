02190d1f344becf31530a04cdd48744b2dc8314c
Zein1 <Zein1@users.noreply.github.com>
2018-11-13 10:38:25 +0100

    Update leplateau.sql
    update DB

--- leplateau.sql
+++ leplateau.sql
@@ -21,8 +21,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `leplateau`
 --
-
--- --------------------------------------------------------
+CREATE DATABASE leplateau ;
+-- 
 
 --
 -- Structure de la table `avis`
