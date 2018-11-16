a73f7848eab024a7097351cb4de8e6baa47e4d36
JeanLou31 <jayetlucas@gmail.com>
2018-11-16 12:30:04 +0100

    jquerry ca marche en fait fallait metre src au lieu de href

--- accueil_societe.php
+++ accueil_societe.php
@@ -13,6 +13,7 @@
 <script type="application/javascript" src="jquery_bootstrap/bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
 <script type="application/javascript" src="jquery_bootstrap/bootstrap-4.1.3-dist/js/jquery-3.3.1.min.js"></script>
 <link rel="stylesheet" type="text/css" href="accueil_societe.css">
+<script src="connexion.js"></script>
 
 <!--mdbootstrap-->
     <!-- Font Awesome -->
