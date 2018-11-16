b05058d6ff166cc5a76bc23a386a4a511d4673f6
Zein1 <Zein1@users.noreply.github.com>
2018-11-16 15:52:33 +0100

    Update traitement_auth.php

--- traitement_auth.php
+++ traitement_auth.php
@@ -12,6 +12,8 @@ catch(Exception $e)
 		die('Erreur : '.$e->getMessage());		// En cas d'erreur, on affiche un message et on arrête tout
 	}
 
-INSERT INTO compte(identifiant, nom, possesseur, console, prix, nbre_joueurs_max, commentaires) VALUES('', 'Battlefield 1942', 'Patrick', 'PC', 45, 50, '2nde guerre mondiale')
+		$bdd->exec('INSERT INTO compte(ID_Compte, identifiant, passwrd, nom, prenom, mail) VALUES(\'\', \'$_POST["identifiant"]\', \'$_POST["mdp"]\', \'$_POST["nom"]\', \'$_POST["prenom"]\', \'$_POST["mail"]\')');
 
+     	header("Location: accueil_societe.php");
+     	exit;
 ?>
