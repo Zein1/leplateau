923d1cc1d932f82864922a54db1a8f8e01e189c1
JeanLou31 <jayetlucas@gmail.com>
2018-11-16 09:36:33 +0100

    connexion
    css bouton connexion

--- accueil_societe.php
+++ accueil_societe.php
@@ -53,31 +53,50 @@
 
 		<!--BOUTON CONNEXION-->
 	   <!-- Button trigger modal -->
-	<button type="button" class="btn btn-red darken-3" id="bouton-principal" data-toggle="modal" data-target="#basicExampleModal"> Connexion </button>      
-	<!-- Modal -->
-	<div class="modal fade" id="basicExampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-       <div class="modal-dialog" role="document">
-         <div class="modal-content" id="Connexion">
-           <div class="modal-header">
-             <h5 class="modal-title" id="exampleModalLabel">Connexion au Plateau</h5>
-             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-               <span aria-hidden="true">&times;</span>
-             </button>
-           </div>
-           <div class="modal-body">                
-           	<form class="oui">
-           	  <p><input type="text" name="identifiant" placeholder="Identifiant"></p>
-           	  <p><input type="password" name="mdp" placeholder="Mot de passe"></p>                
-           	</form>
-           	</div>
+	   <button type="button" class="btn btn-red darken-3" id="bouton-principal" data-toggle="modal" data-target="#basicExampleModal">
+        Connexion
+    </button>
 
-           <div class="modal-footer">
-             <button type="button" class="btn btn-stylish-color" id="annuler" data-dismiss="modal">Annuler</button>
-             <button type="button" class="btn btn-red darken-3" id="bouton-connexion">Connexion</button>
-           </div>
-         </div>
-       </div>
-     </div>
+    <!-- Modal -->
+    <div class="modal fade" id="basicExampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content" id="Connexion">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Connexion au Plateau</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+                <form action="" method="post">
+                    <div class="modal-body">
+
+                        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+
+
+
+                        <div id="formulaire">
+
+                            <p><label for="name"></label><input type="text" name="name" id="name" placeholder="Identifiant"></p>
+                            <div id="error"></div>
+                            <p><label for="mdp"></label><input type="password" name="mdp" id="mdp" placeholder="Mot de passe"></p>
+							<div id="errordeux"></div>
+							
+                        </div>
+
+                    </div>
+                    <div class="modal-footer">
+                        <a href="inscription.html">
+                            <input type="button" class="btn btn-indigo darken-1" id="inscription" value="inscription" />
+                        </a>
+                        <input type="reset" class="btn btn-stylish-color" id="annuler" value="annuler" data-dismiss="modal"/>
+                        <input type="submit" class="btn btn-red darken-3" id="bouton-connexion" value="connexion" />
+                </form>
+            </div>
+        </div>
+    </div>
+    </div>
 	</nav>
 	
 	<!--PARTIE CENTRALE DERNIERS JEUX SORTIS & COUPS DE COEUR-->
