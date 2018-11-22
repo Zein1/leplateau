94b32c71f64bee19660b3779df7360e85a84d533
Zein1 <Zein1@users.noreply.github.com>
2018-11-22 15:47:34 +0100

    deuxieme essai message alerte

--- traitement_auth.php
+++ traitement_auth.php
@@ -1,3 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta charset="utf-8"/>
+<title>Authentification</title>
+<meta name="description" content="Le plateau note les jeux de sociétés récents et anciens."/>
+<link rel="icon" type="image/png" href="image/logoonglet.png"/>
+
+<link href="jquery_bootstrap/bootstrap-4.1.3-dist/css/bootstrap.min.css" rel="stylesheet">
+<script type="application/javascript" src="jquery_bootstrap/bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
+<script type="application/javascript" src="jquery_bootstrap/bootstrap-4.1.3-dist/js/jquery-3.3.1.min.js"></script>
+<link rel="stylesheet" type="text/css" href="accueil_societe.css">
+<script src="connexion.js"></script>
+
+<!--mdbootstrap-->
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.11/css/mdb.min.css" rel="stylesheet">
+    <!-- JQuery -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Bootstrap tooltips -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.5.11/js/mdb.min.js"></script>
+<!--mdbootstrap-->
+
+</head>
+
+<body>
+
 <?php
 
 /*CECI EST UNE PAGE DE TRAITEMENT PHP*/
@@ -32,9 +70,13 @@ catch(Exception $e)
 			?>
 
 			<script> alert("Les mots de passes ne sont pas identiques !"); </script>
-			
+
 			<?php
 			header("Location: inscription.php");
 			exit;
 		}
 ?>
+
+</body>
+
+</html>
